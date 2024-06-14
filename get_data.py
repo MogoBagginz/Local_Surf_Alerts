e72577937eb91cff8079eabed35e10e6aad59851
Murali Wood <mugz.wood@gmail.com>
2024-06-14 18:20:40 +0200

    added fetch_tyde function

--- get_data.py
+++ get_data.py
@@ -15,27 +15,43 @@ WHITESANDS_LNG = -5.2977
 
 # -- send request --
 
-def fetch_surf_forecast(lat, lng, api_key):
-	# Get first hour of today
-	start = arrow.now().floor('day')
+# Get first hour of today
+TIME_AT_START_OF_DAY = arrow.now().floor('day')
 
-	# Get last hour of today
-	end = arrow.now().ceil('day')
+# Get last hour of today
+TIME_AT_END_OF_DAY = arrow.now().ceil('day')
 
+def fetch_surf_forecast(start_time, end_time, lat, lng, api_key):
     response = requests.get(
         'https://api.stormglass.io/v2/weather/point',
         params={
             'lat': lat,
             'lng': lng,
             'params': ','.join(['swellDirection', 'swellHeight', 'swellPeriod', 'windDirection', 'windSpeed']),
-	    	'start': start.to('UTC').timestamp(),  # Convert to UTC timestamp
-    		'end': end.to('UTC').timestamp()  # Convert to UTC timestamp
+	    	'start': start_time.to('UTC').timestamp(),  # Convert to UTC timestamp
+    		'end': end_time.to('UTC').timestamp()  # Convert to UTC timestamp
         },
         headers={
             'Authorization': api_key
         }
     )
     return response.json()
+
+def fetch_tyde(start_time, end_time, lat, lng, api_key)
+	responce = requests.get(
+		'https://api.stormglass.io/v2/tide/extremes/point',
+		params={
+			'lat': lat,
+			'lng': lng,
+			'start': start_time.to('UTC').timestamp(), # Convert to UTC timestamp
+			'end': end_time.to('UTC').timestamp() # Convert to UTC timestamp
+		},
+		headers={
+			'Authorization': api_key
+		}
+	)
+	return responce.json()
+
 
 #TODO create function that creates a new surf_break_configuration
 #TODO create function that changes a surf_break_configuration
