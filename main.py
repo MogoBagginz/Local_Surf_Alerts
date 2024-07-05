1c178410b97a9c6725fe4c2553b9b3fd6a40f445
Murali Wood <mugz.wood@gmail.com>
2024-07-05 16:15:49 +0200

    created spot_conf lib

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 
+import spot_conf
 import process_forcast #TODO replace all forcasts for forecasts
 import fetch_forcast
-import math
 import arrow
-import requests
 import json
 
 from plyer import notification
@@ -24,50 +23,6 @@ TIME_AT_END_OF_DAY = arrow.now().ceil('day')
 
 #TODO create function that creates a new surf_break_configuration
 #TODO create function that changes a surf_break_configuration
-
-# use Object --- Surf_Break_Config(Object)  
-class Surf_Break_Config:
-    def __init__(self, name='Default name', latitude=None, longitude=None,
-                 break_direction=None,ideal_swell_direction=225,
-                 min_wave_energy=0.43, max_onshore_wind_speed=20,
-                 max_offshore_wind_speed=30):
-        self.name                       = name
-        self.latitude                   = latitude
-        self.longitude                  = longitude
-        # break_direction = the angle that the beach or reef break faces in degrees
-        self.break_direction            = break_direction
-        self.ideal_swell_direction      = ideal_swell_direction
-        self.min_wave_energy            = min_wave_energy
-        self.max_onshore_wind_speed     = max_onshore_wind_speed
-        self.max_offshore_wind_speed    = max_offshore_wind_speed
-
-    def save_to_file(self, file_path):
-        with open(file_path, 'w') as file:
-            json.dump(self.__dict__, file)
-
-    def load_from_file(self, file_path):
-        try:
-            with open(file_path, 'r') as file:
-                data = json.load(file)
-                self.name                       = data['name']
-                self.latitude                   = data['latitude']
-                self.longitude                  = data['longitude']
-                self.break_direction            = data['break_direction']
-                self.ideal_swell_direction      = data['ideal_swell_direction']
-                self.min_wave_energy            = data['min_wave_energy']
-                self.max_onshore_wind_speed     = data['max_onshore_wind_speed']
-                self.max_offshore_wind_speed    = data['max_offshore_wind_speed']
-        except FileNotFoundError:
-            print("Configuration file not found. Using default settings.")
-
-    def __str__(self):
-        # Print class name
-        rtnVal = f"\nClass: {self.__class__.__name__}\n\nAttributes:\n"
-
-        # Print instance attributes
-        for attr, value in vars(self).items():
-            rtnVal += f"{attr}: {value}\n"
-        return rtnVal
 
 # function that takes time and location when the surf is good and it saves all the conditions into a file. the file can be used when configuring max and min values for a spot.
 
@@ -89,17 +44,9 @@ def send_notification(title, message):
 if __name__ == "__main__":
     
     # load spot configurations
-
-    whitesands_conf = Surf_Break_Config()
-    whitesands_conf.name = 'whitesands'
-    whitesands_conf.latitude = 51.8969
-    whitesands_conf.longitude = -5.2977
-    whitesands_conf.break_direction = 270
-    whitesands_conf.ideal_swell_direction = 270
-    whitesands_conf.min_wave_energy = process_forcast.get_wave_energy(7,0.3)
-    whitesands_conf.max_offshore_wind_speed = 30
-    whitesands_conf.max_onshore_wind_speed = 20
-    whitesands_conf.save_to_file('whitesands_conf.json')
+    whitesands_conf_path = "whitesands_conf.json"
+    whitesands_conf = spot_conf.Surf_Break_Config()
+    whitesands_conf.load_from_file(whitesands_conf_path)
 
     # spot conditions
     whitesands_conditions = process_forcast.Surf_Break_Conditions()
---
+++ spot_conf.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+import json
+
+# use Object --- Surf_Break_Config(Object)  
+class Surf_Break_Config:
+    def __init__(self, name='Default name', latitude=None, longitude=None,
+                 break_direction=None,ideal_swell_direction=225,
+                 min_wave_energy=0.43, max_onshore_wind_speed=20,
+                 max_offshore_wind_speed=30):
+        self.name                       = name
+        self.latitude                   = latitude
+        self.longitude                  = longitude
+        # break_direction = the angle that the beach or reef break faces in degrees
+        self.break_direction            = break_direction
+        self.ideal_swell_direction      = ideal_swell_direction
+        self.min_wave_energy            = min_wave_energy
+        self.max_onshore_wind_speed     = max_onshore_wind_speed
+        self.max_offshore_wind_speed    = max_offshore_wind_speed
+
+    def save_to_file(self, file_path):
+        with open(file_path, 'w') as file:
+            json.dump(self.__dict__, file)
+
+    def load_from_file(self, file_path):
+        try:
+            with open(file_path, 'r') as file:
+                data = json.load(file)
+                self.name                       = data['name']
+                self.latitude                   = data['latitude']
+                self.longitude                  = data['longitude']
+                self.break_direction            = data['break_direction']
+                self.ideal_swell_direction      = data['ideal_swell_direction']
+                self.min_wave_energy            = data['min_wave_energy']
+                self.max_onshore_wind_speed     = data['max_onshore_wind_speed']
+                self.max_offshore_wind_speed    = data['max_offshore_wind_speed']
+        except FileNotFoundError:
+            print("Configuration file not found. Using default settings.")
+
+    def __str__(self):
+        # Print class name
+        rtnVal = f"\nClass: {self.__class__.__name__}\n\nAttributes:\n"
+
+        # Print instance attributes
+        for attr, value in vars(self).items():
+            rtnVal += f"{attr}: {value}\n"
+        return rtnVal
