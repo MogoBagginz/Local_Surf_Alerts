834fa74a1b175cc1030a124b3bd720588dfb5c32
Murali Wood <mugz.wood@gmail.com>
2024-07-24 11:34:10 +0200

    further testing of phone notification

--- notify_phone.py
+++ notify_phone.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
-
+"""
+notification size : length = 56 characters (characters overflow to next line
+giving you more lines)
+                    hight = 11 maybe but you can add lines using character
+                    overflow
+"""
 import subprocess
 
 def send_notification(title, message):
